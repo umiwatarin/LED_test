928ab02f82d13194c1f4221d417d9c1b2a4f28d8
TERACHI Kaito <kaito.946nct@gmail.com>
2026-01-05 07:48:56 +0900

    Add files via upload

---
+++ ledtest.ino
@@ -0,0 +1,20 @@
+int led = 12;
+int sign = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(sign, INPUT);
+
+  digitalWrite(led, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(sign) == HIGH){
+    digitalWrite(led, LOW);
+  } else {
+    digitalWrite(led, HIGH);
+  }
+}
+
